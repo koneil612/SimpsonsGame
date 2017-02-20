4080c15262c20b55dc000ebd3a7b76373814dce3
koneil612 <kellis1286@gmail.com>
2017-02-20 15:35:02 -0600

    got shooting the zombie working!

--- zombie.js
+++ zombie.js
@@ -173,31 +173,30 @@ window.addEventListener('keydown', function(event) {
 //     }
 // });
 
-function collides(a, b) {
-  return a.x < b.x + b.width &&
-         a.x + a.width > b.x &&
-         a.y < b.y + b.height &&
-         a.y + a.height > b.y;
-}
-
 function handleCollisions() {
   heroBullets.forEach(function(bullet) {
-      if (collides(bullet, zombie)) {
+      if (collision(bullet, zombiePos)) {
         console.log("you shot him!");
+        zombie = "";
         bullet.active = false;
       }
     });
+    if (collision(heroPos, zombiePos)) {
+      console.log("you died SOB");
+      hero.src = "img/wiggum-zombiesm.png";
+      bullet.active = false;
+    }
   }
 
 
-function collision(player) {
-    if (heroPos.x +32 < zombiePos.x){
+function collision(a, b) {
+    if (a.x +32 < b.x){
         return false;
-    } else if (zombiePos.x + 32 < heroPos.x) {
+    } else if (b.x + 32 < a.x) {
       return false;
-  } else if (heroPos.y + 32 < zombiePos.y) {
+  } else if (a.y + 32 < b.y) {
       return false;
-  } else if (zombiePos.y + 32 < heroPos.y ) {
+  } else if (b.y + 32 < a.y ) {
       return false;
     }
     return true;
@@ -231,12 +230,12 @@ function main() {
 
     requestAnimationFrame(main);
 
-    if (collision(zombie)) {
-        hero.src = "img/wiggum-zombiesm.png";
-        console.log("you turned into a zombie");    }
-}
+//     if (collision(zombie)) {
+//         hero.src = "img/wiggum-zombiesm.png";
+//         console.log("you turned into a zombie");    }
+// }
 // zombiePos.x+=1;
 
-
+}
 
 main();
